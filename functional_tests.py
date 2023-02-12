4c77ac542352667a4b8d9b5485f142ef6a4483c7
Kawish Qayyum <kawish@Kara.local>
2023-02-13 01:01:28 +0500

    Add: form with unit tests

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 import unittest
+from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -33,23 +34,35 @@ class NewVisitorTest(unittest.TestCase):
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
         input_box.send_keys(Keys.ENTER)
+        sleep(1)
 
         table = self.browser.find_element(By.ID, "id_list_table")
         rows = table.find_elements(By.TAG_NAME, "tr")
 
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
-        )
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
-        self.fail('Finish the test!')
+        input_box = self.browser.find_element(By.ID, "id_new_item")
+        input_box.send_keys('Use peacock feathers to make a fly')
+        input_box.send_keys(Keys.ENTER)
+
+        sleep(1)
+
+        table = self.browser.find_element(By.ID, "id_list_table")
+        rows = table.find_elements(By.TAG_NAME, "tr")
 
         # The page updates again, and now shows both items on her list
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn(
+            '2: Use peacock feathers to make a fly',
+            [row.text for row in rows]
+        )
 
         # Edith wonders whether the site will remember her list. Then she sees
         # that the site has generated a unique URL for her -- there is some
         # explanatory text to that effect.
+        self.fail('Finish the test!')
 
         # She visits that URL - her to-do list is still there. 
 
--- lists/views.py
+++ lists/views.py
@@ -3,4 +3,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_page(request):
-    return render(request, 'lists/home.html')
+    return render(
+        request=request,
+        template_name='lists/home.html',
+        context={
+            'new_item_text': request.POST.get('item_text'),
+        }
+    )
