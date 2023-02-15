30cb4316a014147d0f871c77ccb2fa07ee1e14d6
Kawish Qayyum <kawish@Kara.local>
2023-02-15 20:28:14 +0500

    Redirect after POST, and show all items in template

--- lists/tests.py
+++ lists/tests.py
@@ -9,9 +9,29 @@ class HomePageTest(TestCase):
         self.assertTemplateUsed(response, 'lists/home.html')
 
     def test_can_save_a_post_request(self):
+        self.client.post('/', data={'item_text': 'A new list'})
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list')
+
+    def test_redirect_after_post(self):
         response = self.client.post('/', data={'item_text': 'A new list'})
-        self.assertIn('A new list', response.content.decode())
-        self.assertTemplateUsed(response, 'lists/home.html')
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    def test_only_save_items_when_necessary(self):
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
+
+    def test_display_all_list_items(self):
+        Item.objects.create(text='first item')
+        Item.objects.create(text='second item')
+
+        response = self.client.get('/')
+
+        self.assertIn('first item', response.content.decode('utf8'))
+        self.assertIn('second item', response.content.decode('utf8'))
 
 
 class ItemModelTest(TestCase):
--- lists/views.py
+++ lists/views.py
@@ -1,12 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from lists.models import Item
 
 
 # Create your views here.
 def home_page(request):
+
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST.get('item_text'))
+        return redirect('/')
+
+    items = Item.objects.all()
     return render(
         request=request,
         template_name='lists/home.html',
-        context={
-            'new_item_text': request.POST.get('item_text'),
-        }
+        context={'items': items}
     )
